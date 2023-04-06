5e62e2633f666af265416e006e08f76daaead62b
Piotr Andrzejak <pandrzejak93@gmail.com>
2023-04-06 16:45:49 +0200

    input output stream example

---
+++ src/main/java/pl/codecool/java_in_out/InputOutputStreamExample.java
@@ -0,0 +1,33 @@
+package pl.codecool.java_in_out;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class InputOutputStreamExample {
+
+    public static void main(String[] args) throws IOException {
+
+
+        FileInputStream fileInputStream = new FileInputStream("src/main/resources/input_output_stream/input_output_stream_data.txt");
+        FileOutputStream fileOutputStream = new FileOutputStream("src/main/resources/input_output_stream/input_output_stream_saved.txt");
+
+        int nextByte = fileInputStream.read();
+        char nextChar = (char) nextByte;
+
+        System.out.println(nextByte);
+        System.out.println(nextChar);
+
+        fileOutputStream.write(nextByte);
+
+        while (nextByte != -1) {
+            nextByte = fileInputStream.read();
+            nextChar = (char) nextByte;
+            System.out.println(nextChar);
+        }
+
+        fileInputStream.close();
+        fileOutputStream.close();
+    }
+}
